c0a39ae03d27ef4523691050d1a51305d00c91f5
jaclynapplegate <jaclynapplegate@gmail.com>
2022-02-16 21:43:07 -0800

    Current Icon API
    added current weather icon API

--- src/java.js
+++ src/java.js
@@ -49,6 +49,11 @@ timeNow.innerHTML = `${hour}:${minutes} PM`;
 // CHANGE CITY
 
 function showWeather(response) {
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let currentIcon = document.querySelector(".currentIcon");
+
   document.querySelector(".city").innerHTML = response.data.name;
   document.querySelector(".currentTemp strong").innerHTML = `${Math.round(
     response.data.main.temp
@@ -56,6 +61,11 @@ function showWeather(response) {
   description.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
+  currentIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  currentIcon.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchForm(event) {
@@ -63,12 +73,11 @@ function searchForm(event) {
   let apiKey = "c41f9f26f03f32443ecf40be638baf03";
   let searchInput = document.querySelector("#search-text");
   let city = document.querySelector(".city");
-  let description = document.querySelector("#description");
-  let humidity = document.querySelector("#humidity");
-  let wind = document.querySelector("#wind");
+
   city.innerHTML = `${searchInput.value}`;
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=imperial`;
   axios.get(apiURL).then(showWeather);
 }
 let form = document.querySelector("form");
 form.addEventListener("submit", searchForm);
+form.addEventListener("enter", searchForm);
